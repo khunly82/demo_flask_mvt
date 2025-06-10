1ea68da48b029c3eb715134143c846afb6f9d377
khun ly <khun.ly@bstorm.be>
2025-06-10 16:34:27 +0200

    change db config

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 SECRET_KEY = 'l0-yh=@v8cn2r*cvn5^-elmmp&o=4k5x3+3q49yjg)cr1u4xrh'
 
 scheme = 'postgresql+psycopg2'
-username = 'postgres'
-password = 'Test!1234'
-host_name = 'localhost'
+username = 'qrufxnufbb'
+password = 'lwZaGxsW4knwS$1b'
+host_name = 'testkhunflask-server'
 port = '5432'
-database_name = 'db_flask_demo'
+database_name = 'testkhunflask-database'
 
 URL_DB = f"{scheme}://{username}:{password}@{host_name}:{port}/{database_name}"
 
